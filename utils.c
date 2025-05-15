85b512967d7b8e1b7bc58da8e85a9d530c82fa77
Yunus Emre Alp <yalp@k1m32s07.42istanbul.com.tr>
2025-05-15 18:08:05 +0300

    m

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: yalp <yalp@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 17:19:11 by yalp              #+#    #+#             */
-/*   Updated: 2025/05/15 18:01:23 by yalp             ###   ########.fr       */
+/*   Updated: 2025/05/15 18:07:34 by yalp             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@ int	is_all_philos_full(t_loop *loop)
 
 void  even_id_philo(t_philosopher *philo)
 {
-	printf("%llu %d is thinking\n",get_time() - philo->loop_con->start_time, philo->id);
 	pthread_mutex_lock(philo->left_fork);
 	printf("%llu %d has taken a fork\n",get_time() - philo->loop_con->start_time, philo->id);
 	pthread_mutex_lock(philo->right_fork);
@@ -55,7 +54,6 @@ void  even_id_philo(t_philosopher *philo)
 
 void  odd_id_philo(t_philosopher *philo)
 {
-	printf("%llu %d is thinking\n",get_time() - philo->loop_con->start_time, philo->id);
 	pthread_mutex_lock(philo->right_fork);
 	printf("%llu %d has taken a fork\n",get_time() - philo->loop_con->start_time, philo->id);
 	pthread_mutex_lock(philo->left_fork);
